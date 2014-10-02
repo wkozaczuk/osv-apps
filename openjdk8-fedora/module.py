09aef3d23fa1e319945fcd3022f41b00326fddb0
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2014-10-02 12:33:37 +0200

    openjdk8-fedora: fix the resulting module
    Fixes https://github.com/cloudius-systems/osv/issues/508.
    
    The root cause was that libjvm was assumed to be found in
    /usr/lib/jvm/java/jre/lib/amd64. The module was missing an appropriate
    symlink for /usr/lib/jvm/java.
    
    I replicated changes made in modules/java/module.py
    
    I also got rid of the GET script by moving logic to Makefile so that
    incremental builds are faster (do not download RPMs every time).
    
    Signed-off-by: Tomasz Grabiec <tgrabiec@cloudius-systems.com>

--- openjdk8-fedora/module.py
+++ openjdk8-fedora/module.py
@@ -5,17 +5,21 @@
 # BSD license as described in the LICENSE file in the top-level directory.
 #
 
-provides = ['java']
-
 from osv.modules.filemap import FileMap
 from osv.modules import api
+import os, os.path
+
+provides = ['java']
 
 api.require('fonts')
+api.require('ca-certificates')
+api.require('libz')
+api.require('josvsym')
 
 usr_files = FileMap()
-
-usr_files.add('${OSV_BASE}/apps/openjdk8-fedora/install/usr/lib/jvm/jre') \
-         .to('/usr/lib/jvm/jre') \
-         .exclude('lib/audio/**') \
-         .exclude('lib/security/cacerts')
-
+usr_files.add('${OSV_BASE}/apps/openjdk8-fedora/install').to('/')
+usr_files.add('${OSV_BASE}/apps/openjdk8-fedora/install/usr/lib/jvm/java') \
+    .to('/usr/lib/jvm/java') \
+    .allow_symlink()
+usr_files.link('/usr/lib/jvm/jre').to('/usr/lib/jvm/java/jre')
+usr_files.link('/usr/lib/jvm/java/jre/lib/security/cacerts').to('/etc/pki/java/cacerts')
