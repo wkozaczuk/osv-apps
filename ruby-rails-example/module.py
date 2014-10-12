deb5dac58fd2f3782a19d1dfb0e1d80239b08ea8
Takuya ASADA <syuu@cloudius-systems.com>
2014-10-13 07:27:19 +0900

    ruby-rails-example: change GEM_HOME for Ruby 2.1.x
    Recent version of bundler changed an install location of gems, follow it.
    
    Signed-off-by: Takuya ASADA <syuu@cloudius-systems.com>

--- ruby-rails-example/module.py
+++ ruby-rails-example/module.py
@@ -1,4 +1,4 @@
 from osv.modules import api
 
 api.require('ruby')
-default = api.run(cmdline="--cwd=/osv_test/ --env=GEM_HOME=/osv_test/vendor/bundle/ruby /ruby.so ./bin/rails server")
+default = api.run(cmdline="--cwd=/osv_test/ --env=GEM_HOME=/osv_test/vendor/bundle/ruby/2.1.0 /ruby.so ./bin/rails server")
