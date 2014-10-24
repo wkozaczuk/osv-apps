eff7b3b4cca0b57c9a9b8fee860eaaaae8545b20
Takuya ASADA <syuu@cloudius-systems.com>
2014-10-25 03:54:23 +0900

    node-express-example: fix execution error
    Signed-off-by: Takuya ASADA <syuu@cloudius-systems.com>

--- node-express-example/module.py
+++ node-express-example/module.py
@@ -2,4 +2,4 @@ from osv.modules import api
 
 api.require('node')
 
-default = api.run(cmdline="/libnode.so /express/examples/hello-world")
+default = api.run(cmdline="--cwd=/express/ /libnode.so ./examples/hello-world")
