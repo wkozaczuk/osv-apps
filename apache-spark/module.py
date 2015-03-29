96ebd3f5bdeb92e004d4dc18c836810d042c0c69
Nadav Har'El <nyh@cloudius-systems.com>
2015-03-29 09:17:54 +0300

    fix "make image=apache-spark"
    Makefile was wrong (build relied on "clean", which on my machine was
    ironically done after the build...), and module.py's command line didn't
    quite work. Fix both of them.
    
    Signed-off-by: Nadav Har'El <nyh@cloudius-systems.com>

--- apache-spark/module.py
+++ apache-spark/module.py
@@ -1,21 +1,5 @@
 from osv.modules import api
 
-default = api.run_java(
-    jvm_args=[
-        '-Xms512m',
-        '-Xmx512m',
-        '-Dscala.usejavacp=true',
-        '--class org.apache.spark.repl.Main',
-    ],
-    classpath=[
-        '/spark/conf',
-        '/spark/lib/spark-assembly-1.3.0-hadoop2.4.0.jar',
-        '/spark/lib/datanucleus-core-3.2.10.jar',
-        '/spark/lib/datanucleus-rdbms-3.2.9.jar',
-        '/spark/lib/datanucleus-api-jdo-3.2.6.jar',
-    ],
-    args=[
-        'org.apache.spark.deploy.SparkSubmit',
-        'spark-shell',
-    ]
-)
+api.require('java')
+
+default = api.run("--cwd=/spark /java.so -Xms512m -Xmx512m -cp /spark/conf:/spark/lib/spark-assembly-1.3.0-hadoop2.4.0.jar:/spark/lib/datanucleus-core-3.2.10.jar:/spark/lib/datanucleus-rdbms-3.2.9.jar:/spark/lib/datanucleus-api-jdo-3.2.6.jar -Dscala.usejavacp=true org.apache.spark.deploy.SparkSubmit --class org.apache.spark.repl.Main spark-shell")
