aaffc24c2cbf0f139cf54d1d126b2f0401372639
Arun Babu Neelicattu <arun.neelicattu@gmail.com>
2015-04-13 10:49:35 +1000

    apache-activemq: Add initial version (5.11.1)
    Signed-off-by: Arun Babu Neelicattu <arun.neelicattu@gmail.com>
    Signed-off-by: Nadav Har'El <nyh@cloudius-systems.com>

---
+++ apache-activemq/module.py
@@ -0,0 +1,18 @@
+from osv.modules import api
+
+default = api.run_java(
+    jvm_args=[
+        '-Xms1G',
+        '-Xmx1G',
+        '-Djava.security.auth.login.config=/activemq/conf/login.config',
+        '-Dcom.sun.management.jmxremote',
+        '-Djava.awt.headless=true',
+        '-Djava.io.tmpdir=/activemq/tmp',
+        '-Dactivemq.classpath=/activemq/conf',
+        '-Dactivemq.home=/activemq',
+        '-Dactivemq.base=/activemq',
+        '-Dactivemq.conf=/activemq/conf',
+        '-Dactivemq.data=/activemq/data',
+    ],
+    args=['-jar /activemq/bin/activemq.jar start']
+)
