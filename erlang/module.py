d66b6b9773a8e5cc64a57f36d4fa5c6002b20ac7
bhuztez <bhuztez@gmail.com>
2015-05-12 15:11:12 +0800

    erlang: enable epmd

--- erlang/module.py
+++ erlang/module.py
@@ -1,3 +1,3 @@
 from osv.modules import api
 
-default = api.run(cmdline="--env=BINDIR=/usr/lib64/erlang/erts-6.2/bin /usr/lib64/erlang/erts-6.2/bin/beam.smp -K true -- -root /usr/lib64/erlang -progname erl -- -home / --")
+default = api.run(cmdline="--env=BINDIR=/usr/lib64/erlang/erts-6.2/bin --env=HOME=/ /usr/lib64/erlang/erts-6.2/bin/erlexec +K true -name foo@127.0.0.1")
