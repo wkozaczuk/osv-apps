611fb8de5d67b208ead20f866f0ab02c4f03e029
bhuztez <bhuztez@gmail.com>
2015-05-13 16:54:36 +0800

    add elixir module

--- erlang/module.py
+++ erlang/module.py
@@ -1,3 +1,5 @@
 from osv.modules import api
 
+api.require("openssl")
+api.require("libz")
 default = api.run(cmdline="/usr/lib64/erlang.so -env HOME / /etc/erlang/vm.args /etc/default/erlang/vm.args")
