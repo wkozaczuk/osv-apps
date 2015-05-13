8343b053a9b44b70aeca3ea8e480ed8f945dafbc
bhuztez <bhuztez@gmail.com>
2015-05-13 20:59:43 +0800

    erlang: enable termcap

--- erlang/module.py
+++ erlang/module.py
@@ -2,4 +2,5 @@ from osv.modules import api
 
 api.require("openssl")
 api.require("libz")
+api.require("ncurses")
 default = api.run(cmdline="/usr/lib64/erlang.so -env HOME / /etc/erlang/vm.args /etc/default/erlang/vm.args")
