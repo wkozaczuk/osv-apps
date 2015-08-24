cd7d22aef61e511902cbb74ab57788bb86f23ae7
David Jorm <david.jorm@gmail.com>
2015-08-24 18:47:23 +1000

    Upgraded ONOS to 1.2.1
    Signed-off-by: David Jorm <david.jorm@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- onos/module.py
+++ onos/module.py
@@ -4,10 +4,10 @@ from osv.modules import api
 default = api.run_java(
     args=['org.apache.karaf.main.Main'],
     classpath=[
-        '/onos/apache-karaf-3.0.2/lib/karaf-jaas-boot.jar',
-        '/onos/apache-karaf-3.0.2/lib/karaf.jar',
-        '/onos/apache-karaf-3.0.2/lib/karaf-jmx-boot.jar',
-        '/onos/apache-karaf-3.0.2/lib/karaf-org.osgi.core.jar'
+        '/onos/apache-karaf-3.0.3/lib/karaf-jaas-boot.jar',
+        '/onos/apache-karaf-3.0.3/lib/karaf.jar',
+        '/onos/apache-karaf-3.0.3/lib/karaf-jmx-boot.jar',
+        '/onos/apache-karaf-3.0.3/lib/karaf-org.osgi.core.jar'
     ],
     jvm_args=[
         '-Xms128M',
@@ -17,15 +17,15 @@ default = api.run_java(
         '-Dcom.sun.management.jmxremote',
         '-Djava.endorsed.dirs=/usr/lib/jvm/java/jre/lib/endorsed'
         ':/usr/lib/jvm/java/lib/endorsed'
-        ':/onos/apache-karaf-3.0.2/lib/endorsed',
-        '-Djava.ext.dirs=/usr/lib/jvm/java/jre/lib/ext:/usr/lib/jvm/java/lib/ext:/onos/apache-karaf-3.0.2/lib/ext',
-        '-Dkaraf.instances=/onos/apache-karaf-3.0.2/instances',
-        '-Dkaraf.home=/onos/apache-karaf-3.0.2',
-        '-Dkaraf.base=/onos/apache-karaf-3.0.2',
-        '-Dkaraf.data=/onos/apache-karaf-3.0.2/data',
-        '-Dkaraf.etc=/onos/apache-karaf-3.0.2/etc',
-        '-Djava.io.tmpdir=/onos/apache-karaf-3.0.2/data/tmp',
-        '-Djava.util.logging.config.file=/onos/apache-karaf-3.0.2/etc/java.util.logging.properties',
+        ':/onos/apache-karaf-3.0.3/lib/endorsed',
+        '-Djava.ext.dirs=/usr/lib/jvm/java/jre/lib/ext:/usr/lib/jvm/java/lib/ext:/onos/apache-karaf-3.0.3/lib/ext',
+        '-Dkaraf.instances=/onos/apache-karaf-3.0.3/instances',
+        '-Dkaraf.home=/onos/apache-karaf-3.0.3',
+        '-Dkaraf.base=/onos/apache-karaf-3.0.3',
+        '-Dkaraf.data=/onos/apache-karaf-3.0.3/data',
+        '-Dkaraf.etc=/onos/apache-karaf-3.0.3/etc',
+        '-Djava.io.tmpdir=/onos/apache-karaf-3.0.3/data/tmp',
+        '-Djava.util.logging.config.file=/onos/apache-karaf-3.0.3/etc/java.util.logging.properties',
         '-Dkaraf.startLocalConsole=true',
         '-Dkaraf.startRemoteShell=true',
     ],
