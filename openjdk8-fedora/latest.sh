2320f2facc5c468064a0221f1345a6ff0cf2a974
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2016-11-29 00:14:41 -0500

    Updated openjdk* apps

--- openjdk8-fedora/latest.sh
+++ openjdk8-fedora/latest.sh
@@ -6,4 +6,4 @@ arch=$2
 
 letter=${package:0:1}
 
-wget -qO- http://mirrors.kernel.org/fedora/development/rawhide/Everything/x86_64/os/Packages/$letter/ | grep "$package-[0-9].*$arch" | sed -e "s/<a href\=\"$package-\(.*\)\.$arch\.rpm\".*/\1/g"
+wget -t 1 -qO- http://mirrors.kernel.org/fedora/development/rawhide/Everything/x86_64/os/Packages/$letter/ | grep "$package-[0-9].*$arch" | sed -e "s/<a href\=\"$package-\(.*\)\.$arch\.rpm\".*/\1/g"
--- openjdk8-zulu-compact-common/latest.sh
+++ openjdk8-zulu-compact-common/latest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget -qO- http://www.azul.com/downloads/zulu/zulu-linux/ | grep "jdk8.*linux.*64.tar.gz" | grep -o "http:.*tar.gz" | head -1
+wget -qO- http://www.azul.com/downloads/zulu/zulu-linux | grep "jdk8.*linux.*64.tar.gz\"" | grep -o "http:.*tar.gz" | head -1
