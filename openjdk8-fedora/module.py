36bfb65d71569b24bc462cd025ee8d5e886a7bdb
Nadav Har'El <nyh@scylladb.com>
2017-06-08 15:30:08 +0300

    openjdk8-fedora: simplify preparation of image content
    Just treat "install/" as the contents of what will be installed on the
    image. No need for convoluted python magic in module.py to override what
    we put in the install/ directory - it only complicates matters.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>

--- openjdk8-fedora/module.py
+++ openjdk8-fedora/module.py
@@ -12,7 +12,4 @@ provides = ['java']
 
 usr_files = FileMap()
 usr_files.add('${OSV_BASE}/apps/openjdk8-fedora/install').to('/').allow_symlink()
-usr_files.add('${OSV_BASE}/apps/openjdk8-fedora/install/usr/lib/jvm/java') \
-    .to('/usr/lib/jvm/java') \
-    .allow_symlink()
 usr_files.link('/usr/lib/jvm/jre').to('/usr/lib/jvm/java/jre')
