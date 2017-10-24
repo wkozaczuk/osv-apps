8dac3e45ccdd0d7cc30c2df5f5feae2f9f38ee43
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2017-10-24 07:51:22 -0400

    Support Java GA 9
    This patch provides new app to run Java 9 apps and replaces
    openjdk9-ea-java-base. It uses jlink tool to create minimal JRE
    with single java-base module (please note jlink --add-modules java.base argument).
    One would need more Java 9 modules to run more sophisticated apps.
    
    scripts/build image=openjdk9-java-base,java-example
    
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>
    Message-Id: <1508845882-5576-1-git-send-email-jwkozaczuk@gmail.com>

--- openjdk9-java-base/module.py
+++ openjdk9-java-base/module.py
@@ -12,8 +12,7 @@ api.require('java-non-isolated')
 provides = ['java']
 
 usr_files = FileMap()
-usr_files.add('${OSV_BASE}/apps/openjdk9-ea-java-base/install').to('/').allow_symlink()
-usr_files.add('${OSV_BASE}/apps/openjdk9-ea-java-base/install/usr/lib/jvm/java') \
+usr_files.add('${OSV_BASE}/apps/openjdk9-java-base/install').to('/').allow_symlink()
+usr_files.add('${OSV_BASE}/apps/openjdk9-java-base/install/usr/lib/jvm/java') \
     .to('/usr/lib/jvm/java') \
     .allow_symlink()
-usr_files.link('/usr/lib/jvm/jre').to('/usr/lib/jvm/java/jre')
