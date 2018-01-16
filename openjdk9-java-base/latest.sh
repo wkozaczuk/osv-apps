1ffb420ab38d002e80b9fe8a0e28bcbd4999c889
geraldo netto <geraldonetto@gmail.com>
2018-01-16 17:42:46 -0200

    openjdk9-java-base/latest.sh: use wget -c
    Signed-off-by: geraldo netto <geraldonetto@gmail.com>
    Message-Id: <1516131789-8559-34-git-send-email-geraldonetto@gmail.com>

--- openjdk9-java-base/latest.sh
+++ openjdk9-java-base/latest.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -qO- http://jdk.java.net/9/ | grep "http://download.java.net/java/GA/jdk9.*linux-x64_bin.tar.gz" | grep -o "openjdk-.*linux" | grep -o '9\.[0-9]\+\.[0-9]\+'
+wget -c -qO- http://jdk.java.net/9/ | grep "http://download.java.net/java/GA/jdk9.*linux-x64_bin.tar.gz" | grep -o "openjdk-.*linux" | grep -o '9\.[0-9]\+\.[0-9]\+'
