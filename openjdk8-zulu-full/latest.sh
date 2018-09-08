d2f5b2f1a0aede875b015e6a5eac8b788d2024c3
geraldo netto <geraldonetto@gmail.com>
2018-09-07 22:17:53 -0300

    openjdk8-zulu-full: reuse openjdk8-zulu-compact-common
    reuse openjdk8-zulu-compact-common/latest.sh in order to avoid duplicated script
    now, all zulu profiles (compact1, 2, 3, 3 with beans, full) are using a single source
    
    Signed-off-by: geraldo netto <geraldonetto@gmail.com>
    Message-Id: <20180908011753.2073-3-geraldonetto@gmail.com>

--- openjdk8-zulu-full/latest.sh
+++ openjdk8-zulu-full/latest.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-wget -c -qO- http://www.azul.com/downloads/zulu/zulu-linux | grep -o '<a href=['"'"'"][^"'"'"']*['"'"'"]' | grep "jdk8.*linux.*64.tar.gz\"" | cut -d'"' -f2 | head -1
+. ../openjdk8-zulu-compact-common/latest.sh
