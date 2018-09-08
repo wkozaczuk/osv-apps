e31e0764f73616e12b9fd27e3eb040478450f20c
geraldo netto <geraldonetto@gmail.com>
2018-09-07 22:17:52 -0300

    improve url parsing for openjdk8-zulu-compact-common
    for some reason the parsing of openjdk8 (zulu full and compact profiles) was broken
    so, based on [1] we first extract all <a href> elements and then we extract the latest version available
    
    [1] https://stackoverflow.com/questions/1881237/easiest-way-to-extract-the-urls-from-an-html-page-using-sed-or-awk-only
    
    Signed-off-by: geraldo netto <geraldonetto@gmail.com>
    Message-Id: <20180908011753.2073-2-geraldonetto@gmail.com>

--- openjdk8-zulu-compact-common/latest.sh
+++ openjdk8-zulu-compact-common/latest.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget -c -qO- http://www.azul.com/downloads/zulu/zulu-linux | grep "jdk8.*linux.*64.tar.gz\"" | grep -o "http:.*tar.gz" | head -1
+wget -c -qO- http://www.azul.com/downloads/zulu/zulu-linux | grep -o '<a href=['"'"'"][^"'"'"']*['"'"'"]' | grep "jdk8.*linux.*64.tar.gz\"" | cut -d'"' -f2 | head -1
