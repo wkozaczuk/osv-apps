52e2ca62f63273866e8c8b3912c0d7417c2dd10d
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2018-09-09 00:15:00 -0400

    Fixed openjdk9-zulu-java-base app latest.sh script
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>

--- openjdk9-zulu-java-base/latest.sh
+++ openjdk9-zulu-java-base/latest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DOWNLOAD_URL=`wget -c -qO- http://www.azul.com/downloads/zulu/zulu-linux | grep "jdk9.*linux.*64.tar.gz\"" | grep -o "https:.*tar.gz" | head -n 1`
+DOWNLOAD_URL=`wget -c -qO- http://www.azul.com/downloads/zulu/zulu-linux | grep -o '<a href=['"'"'"][^"'"'"']*['"'"'"]' | grep "jdk9.*linux.*64.tar.gz\"" | grep -o "https:.*tar.gz" | head -n 1`
 VERSION=`echo $DOWNLOAD_URL | grep -o "zulu9.*linux_x64"`
 
 if [ "$1" = "version" ]; then
