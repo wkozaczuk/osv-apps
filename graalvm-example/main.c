50eb683f44d66e3a5d335eed0797eb5e5e752b13
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2019-02-24 00:54:36 -0500

    Updated graalvm-example to graalvm 1.0 RC12
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>

--- graalvm-example/main.c
+++ graalvm-example/main.c
@@ -13,7 +13,7 @@ int main(int argc, char **argv) {
   graal_isolate_t *isolate = NULL;
   graal_isolatethread_t *thread = NULL;
   
-  if (graal_create_isolate(NULL, &isolate) != 0 || (thread = graal_current_thread(isolate)) == NULL) {
+  if (graal_create_isolate(NULL, &isolate, &thread) != 0) {
     fprintf(stderr, "initialization error\n");
     return 1;
   }
