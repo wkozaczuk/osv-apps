5ef2b1ffb70d3cd6477a8258a739df76cb389247
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2019-07-15 17:39:52 -0400

    openjdk: updated latest scripts to pull openjdk zulu distributions from other new location
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>

--- openjdk12-zulu-java-base/latest.sh
+++ openjdk12-zulu-java-base/latest.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-DOWNLOAD_URL=`wget -c -qO- http://www.azul.com/downloads/zulu/zulu-linux | grep -o '<a href=['"'"'"][^"'"'"']*['"'"'"]' | grep "jdk12.*linux.*64.tar.gz\"" | grep -o "https:.*tar.gz" | head -n 1`
+DOWNLOAD_URL_SUFFIX=`wget -c -qO- https://cdn.azul.com/zulu/bin/ | grep -o '<a href=['"'"'"][^"'"'"']*['"'"'"]' | grep "jdk12.*linux.*64.tar.gz\"" | grep -o "zulu.*tar.gz" | tail -n 1`
+DOWNLOAD_URL="https://cdn.azul.com/zulu/bin/$DOWNLOAD_URL_SUFFIX"
 VERSION=`echo $DOWNLOAD_URL | grep -o "zulu12.*linux_x64"`
 
 if [ "$1" = "version" ]; then
--- openjdk8-zulu-compact-common/latest.sh
+++ openjdk8-zulu-compact-common/latest.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-wget -c -qO- http://www.azul.com/downloads/zulu/zulu-linux | grep -o '<a href=['"'"'"][^"'"'"']*['"'"'"]' | grep "jdk8.*linux.*64.tar.gz\"" | cut -d'"' -f2 | head -1
+DOWNLOAD_URL_SUFFIX=`wget -c -qO- https://cdn.azul.com/zulu/bin/ | grep -o '<a href=['"'"'"][^"'"'"']*['"'"'"]' | grep "jdk8.*linux.*64.tar.gz\"" | grep -o "zulu.*tar.gz" | tail -n 1`
+DOWNLOAD_URL="https://cdn.azul.com/zulu/bin/$DOWNLOAD_URL_SUFFIX"
+echo $DOWNLOAD_URL
