ee9058a7160089020885acf647ca6278559152d1
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2019-09-03 23:45:48 -0400

    elasticsearch: upgraded to latest 7.3.1
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>

--- elasticsearch/module.py
+++ elasticsearch/module.py
@@ -1,24 +1,13 @@
 from osv.modules import api
 
-api.require('java8')
+api.require('libz')
 
-default = api.run(
-' --cwd=/elasticsearch '
-'  /java.so '
-'  -Xms256m '
-'  -Xmx1g '
-'  -Djava.awt.headless=true '
-'  -XX:+UseParNewGC '
-'  -XX:+UseConcMarkSweepGC '
-'  -XX:CMSInitiatingOccupancyFraction=75 '
-'  -XX:+UseCMSInitiatingOccupancyOnly '
-'  -XX:+HeapDumpOnOutOfMemoryError '
-'  -XX:+DisableExplicitGC '
-'  -Dfile.encoding=UTF-8 '
-'  -Djna.nosys=true '
-'  -Des.insecure.allow.root=true '
-'  -Des.path.home=/elasticsearch '
-'  -Des.foreground=true '
-' -cp /elasticsearch/lib/elasticsearch-2.4.2.jar:/elasticsearch/lib/* '
-' org.elasticsearch.bootstrap.Elasticsearch '
-' start ')
+default = api.run( '--env=HOSTNAME=localhost /elasticsearch/java/bin/java'
+'  -Xms1g -Xmx1g -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=75 -XX:+UseCMSInitiatingOccupancyOnly '
+'  -Des.networkaddress.cache.ttl=60 -Des.networkaddress.cache.negative.ttl=10 -XX:+AlwaysPreTouch -Xss1m -Djava.awt.headless=true '
+'  -Dfile.encoding=UTF-8 -Djna.nosys=true -XX:-OmitStackTraceInFastThrow -Dio.netty.noUnsafe=true -Dio.netty.noKeySetOptimization=true '
+'  -Dio.netty.recycler.maxCapacityPerThread=0 -Dlog4j.shutdownHookEnabled=false -Dlog4j2.disable.jmx=true -Djava.io.tmpdir=/tmp '
+'  -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=data '
+'  -Djava.locale.providers=COMPAT -Dio.netty.allocator.type=unpooled -XX:MaxDirectMemorySize=536870912 '
+'  -Des.path.home=/elasticsearch -Des.path.conf=/elasticsearch/config -Des.distribution.flavor=default -Des.distribution.type=tar -Des.bundled_jdk=true '
+'  -cp /elasticsearch/lib/* org.elasticsearch.bootstrap.Elasticsearch !')
