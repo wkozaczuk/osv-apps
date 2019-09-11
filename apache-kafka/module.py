d16568625757d4d53f86baaff33443a9078314e7
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2019-09-11 09:00:33 -0400

    kafka: upgrade kafka to 2.3.0 and added targets for native java executables
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>

--- apache-kafka/module.py
+++ apache-kafka/module.py
@@ -2,9 +2,7 @@ from osv.modules import api
 
 api.require('java8')
 
-default = api.run(
-    '--cwd=/kafka '
-    '/java.so '
+kafka_cmd = (
     '-Xmx1G '
     '-Xms1G '
     '-XX:+UseParNewGC '
@@ -14,15 +12,25 @@ default = api.run(
     '-XX:+DisableExplicitGC '
     '-Djava.awt.headless=true '
     '-Xloggc:/kafka/logs/kafkaServer-gc.log '
-    '-verbose:gc '
-    '-XX:+PrintGCDetails '
-    '-XX:+PrintGCDateStamps '
-    '-XX:+PrintGCTimeStamps '
     '-Dcom.sun.management.jmxremote '
     '-Dcom.sun.management.jmxremote.authenticate=false '
     '-Dcom.sun.management.jmxremote.ssl=false '
     '-Dkafka.logs.dir=/kafka/logs '
     '-Dlog4j.configuration=file:/kafka/config/log4j.properties '
     '-cp :/kafka/libs/* '
-    'kafka.Kafka /kafka/config/server.properties'
-)
+    'kafka.Kafka /kafka/config/server.properties')
+
+zookeeper_cmd = (
+    '-Xmx512M -Xms512M -server -XX:+UseG1GC -XX:MaxGCPauseMillis=20 '
+    '-XX:InitiatingHeapOccupancyPercent=35 -XX:+ExplicitGCInvokesConcurrent '
+    '-Djava.awt.headless=true -Xloggc:/kafka/logs/zookeeper-gc.log '
+    '-Dcom.sun.management.jmxremote '
+    '-Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false '
+    '-Dkafka.logs.dir=/kafka/logs -Dlog4j.configuration=file:./kafka/config/log4j.properties '
+    '-cp :/kafka/libs/* '
+    'org.apache.zookeeper.server.quorum.QuorumPeerMain /kafka/config/zookeeper.properties')
+
+default = api.run('/java.so ' + kafka_cmd)
+native = api.run('/usr/bin/java ' + kafka_cmd)
+
+zookeeper = api.run('/usr/bin/java ' + zookeeper_cmd)
