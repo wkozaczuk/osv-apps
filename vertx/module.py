295fac8bedee2d4041ef712912b378f8ac8b4389
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2019-09-12 08:24:17 -0400

    vertx: added runs for native java
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>

--- vertx/module.py
+++ vertx/module.py
@@ -2,4 +2,6 @@ from osv.modules import api
 
 api.require('java')
 
-default = api.run('/java.so -Xms64m -Xmx64m -Dvertx.disableDnsResolver=true -jar HelloWorld.jar')
+java_cmd = '-Xms64m -Xmx64m -Dvertx.disableDnsResolver=true -jar HelloWorld.jar'
+default = api.run('/java.so ' + java_cmd)
+native = api.run('/usr/bin/java ' + java_cmd)
