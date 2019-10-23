333517c732d612953d16c97803bca954683ac13f
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2019-10-23 08:37:05 -0400

    One more test

--- python-from-host/test.sh
+++ python-from-host/test.sh
@@ -3,4 +3,4 @@
 THIS_DIR=$(readlink -f $(dirname $0))
 
 $THIS_DIR/../../scripts/tests/test_app.py -e \
-  "--env=TERM=unknown /python -c \"print('Hello from Python on OSv')\"" --line 'Hello from Python on OSv'
+  "--env=TERM=unknown /python -c \"import sys; print('Hello from Python %d.%d.%d on OSv' % (sys.version_info.major,sys.version_info.minor,sys.version_info.micro))\"" --line 'Hello from Python'
