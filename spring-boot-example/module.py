b9c60de1728a1f198fe376a0fa2d6143acec4cd2
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2019-10-23 22:14:47 -0400

    spring-boot: fixed incorrect command line
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>

--- spring-boot-example/module.py
+++ spring-boot-example/module.py
@@ -4,4 +4,4 @@ api.require('java')
 
 java_cmd = "-Xms128m -Xmx128m -jar spring-boot-2-rest-service-basic.jar"
 default = api.run('/java.so ' + java_cmd)
-native = api.run('/usr/java/bin ' + java_cmd)
+native = api.run('/usr/bin/java ' + java_cmd)
