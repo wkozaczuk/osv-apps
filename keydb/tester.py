95efeb53184ebdb2aa5ab2e3f326ca70a5923438
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2019-10-24 21:45:23 -0400

    keydb: make it testable
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>

---
+++ keydb/tester.py
@@ -0,0 +1,14 @@
+import sys
+import subprocess
+
+success = False
+
+print("Started keydb test ...")
+try:
+   output = subprocess.check_output(["redis-benchmark", "-t", "set,lpush", "-n", "100000", "-q"], stderr=subprocess.STDOUT)
+   print(output)
+
+   if 'requests per second' in output:
+      success = True
+except subprocess.CalledProcessError as err:
+   print(err.output)
